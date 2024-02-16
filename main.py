3c65c214c14f490155d5ac69dd601a8de8ecca58
omen blue <no@no.com>
2024-02-16 19:17:27 +0300

    Подключен gdrive

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QLabel, QTabWidget
 from amocrm.v2 import tokens, Lead as _Lead, custom_field
 import openpyxl
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
-CRM_LIVE_IDS = [511733, 511795, 511763, 512185, 511767, 511769, 511771, 511773, 511777, 511775, 511779, 511799, 512127, 519487, 519657, 511785, 511787, 512189, 512187, 517861]
+
+
+
+import sys
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+
+CRM_LIVE_IDS = [511733, 511795, 511763, 512185, 511767, 511769, 511771, 511773, 511777, 511775, 511779, 511799,
+                512127, 519487, 519657, 511785, 511787, 512189, 512187, 517861, 511781, 511853, 512005, 511999,
+                512965, 512001, 511861, 511855, 517851, 517853, 517863, 517857, 517859, 517865]
+
 class Lead(_Lead):
     event_type = custom_field.UrlCustomField("Тип мероприятия")
     broadcast_type = custom_field.UrlCustomField("Вид трансляции")
@@ -31,43 +46,33 @@ class Lead(_Lead):
     additional_rent = custom_field.UrlCustomField("Доп. аренда")
     additional = custom_field.UrlCustomField("Дополнительно")
     has_memo_was_sent_to_customer = custom_field.CheckboxCustomField("Заказчику отправлена памятка")
-    has_access_to_channels= custom_field.CheckboxCustomField("Получены доступы к каналу/соц-сетям")
-    has_broadcast_scheduled= custom_field.CheckboxCustomField("Трансляция запланирована")
+    has_access_to_channels = custom_field.CheckboxCustomField("Получены доступы к каналу/соц-сетям")
+    has_broadcast_scheduled = custom_field.CheckboxCustomField("Трансляция запланирована")
     has_speakers_list = custom_field.CheckboxCustomField("Получен список спикеров для подписей")
-    has_speakers_regalia= custom_field.CheckboxCustomField("Получена таблица удаленных спикеров")
+    has_speakers_regalia = custom_field.CheckboxCustomField("Получена таблица удаленных спикеров")
     has_graphics = custom_field.CheckboxCustomField("Создана/получена эфирная графика")
     has_and_agreed_GT_titles = custom_field.CheckboxCustomField("Созданы и согласованы титры GT Tittle")
 
-
-def get_field_values(lead, field_id):
-    data = vars(lead)
-    values = []
-    for field in data['_data']['custom_fields_values']:
-        if field['field_id'] == field_id:
-            for value in field['values']:
-                values.append(value['value'])
-    return values
-
-
-if __name__ == '__main__':
+def get_lead(lead_name: str) -> Lead:
     tokens.default_token_manager(
-        client_id="d3e149f4-19d1-4de3-92f4-d50e0113fac1",
-        client_secret="6P098dcLVPF9lrVm8tDVJsQbrEgQUqcD29QK33s63pn3EZiywt1cj06vYc4txjuZ",
+        client_id="5ebcd675-ea67-46ec-8a2b-b60980b973e1",
+        client_secret="pzIj58pVFqICAdgeWxCpAQS2LBtyOhL8t8i6dGIEP2IdNusugFSmo28UfeaTtDou",
         subdomain="zolotovstudio",
         redirect_url="https://ya.ru",
         storage=tokens.FileTokensStorage(),  # by default FileTokensStorage
     )
 
-    # tokens.default_token_manager.init(code="def5020039c1596b9a95a2f5cac6ce176835b3ceb70e0fdd0199dd9cd711953001338d477829aebbddba13db1b27265b0bcebe4b77e5d6791035debcec8e03f8dc3867780b1e958d1041bdb731ef0587784e38824aa2a1c6a7e9a11c6bb09e4c4136c7a7b99500c588afbc202cdd0e4467aeffba8141636011d3af9c8c416876ade4506dc42fcf3291f6fd7fe4950db9050e053dd8d0edfa7164523eba197b10e663b28a3c84c61a3db7bdbd9e09bb412ed5fa6b8c17a6523fe76d73ddeca71f6002dbe3eb1b65db75f6ca0356104570477e11f3049ffa4e07510317663f8225801823a1cffcc080183b6c8ccb2c30aba8b9a4dc701447c861c981150545e6db097437f8d70e0afa77e7da5dba720044656e93937375c37df22d49321f04715282412d59bc75af162526ece2046ceef062319a1b763d40949a2b4e5478d3d4ce152429c41ff8c69e580d6820eadd8058aefe9c1fb1c064a94bbc3c3228c7036b7a06b4d9b1cb488c835fd5332729c2eca7bfab79a80b3d98d422994a5b2a720c2c6a28e0144cbd982ee1360bfd30654fff4c1cee614521958b7669206c4b7942f7049e06ce42105c18a78573ab5faaba22febcff2e05289ece44e96963ae1987572154bc22bb1951ab1fac08c8637d1229b2dfc5c7390800b12c2c5330",
-    #                                   skip_error=True)
+    tokens.default_token_manager.init(
+        code="def502003d0170413a847b9cb605552607deb30f28f6740a8ec361c4ef2f16a3e89956567e246b568cc6967ca2e477aab60e678329fcbd1beec23181b323febf049efffeb55d260d6022f21c042c56cb8b23336675c28dd18be936a89865c3c463832e1034c4f6f059ec3559e7fc572e1ea1a7ec2cb6c8acf590cca5e62aef725d400db764d329e89d02aeba46981f46f9477283e105c0e4325c66c607aa1c65679395382fa6b176edf7c2fcddd4fc308f48f97677ada7d5ae99247a30665d00681f7ebb0e0a21468ec5edc6b1f883eaabfe31a254933db40f403dd020601c58ec9fab3d7581ce86c7f573a2dc76aa9f7631dd28856b9a576f3da5f799a51cc84f24e7fa4d9bb4835b2d521e27f94f1a9858c47540a5d37b8ec4824baf3abf0fbfd530cbaaeec7e77fabee395858485bc427ca94460054b4161751de3a253ff829ac1a56a8608d935a0533ae3d45e93d08b80cc25aa43ae5d24e4c2e11854702e85e7527db5ff82e20f265e2d98d8138e17dfbb133f3b7be39c6d44f02eaaeedf79d1dee1dd6b4d0b61d2c1c9cc1a871b728d115391cd4f71a9f6ec5bd4c3409fbf3069a264a791145dec1b5f2169aafddc19748297fd627419ac394e70f6e91dc579a6a54085587f350785a8ff98010e66ed442e2adb17f176a8a68e5",
+        skip_error=True)
 
-    leads = Lead.objects.all()
+    # leads = Lead.objects.all()
 
     # for lead in leads:
     #     print(lead.id, lead.name)
-
-
-    lead = Lead.objects.get(query='ТЕСТ ДЛЯ ДЖУ 2')
+    # lead_name = self.tab1_input.text()
+    lead = Lead.objects.get(query=lead_name)
+    print('взята сделка из: [' + lead.name + ']')
     # lead.name = 'ТЕСТ ДЛЯ ДЖУ 2'
     # lead.save()
     # print(lead.event_type)
@@ -106,14 +111,13 @@ if __name__ == '__main__':
     # print(lead.has_and_agreed_GT_titles)
 
     # print(get_field_values(lead, 511795))
+    return lead
 
-
-
-    print(CRM_LIVE_IDS)
+def optimize_lead_to_data(lead:Lead):
 
     fields_data = {}
     data = vars(lead)
-
+    # print(data)
     # Проход по списку custom_fields_values и запись полей field_name и value в словарь
     for field in data['_data']['custom_fields_values']:
         if field['field_id'] in CRM_LIVE_IDS:
@@ -122,8 +126,11 @@ if __name__ == '__main__':
             values = [str(value['value']) for value in field['values']]  # Преобразуем значения в строки
             value = ', '.join(values)  # Объединяем значения в строку, разделенную запятыми
             fields_data[field_id] = {'field_name': field_name, 'value': value}
-
-    print(fields_data)
+    return fields_data
+def lead_to_excel(*, lead_name, project_name):
+    lead = get_lead(lead_name)
+    fields_data = optimize_lead_to_data(lead)
+    # print(fields_data)
 
     # Создание нового файла Excel
     workbook = openpyxl.Workbook()
@@ -141,4 +148,106 @@ if __name__ == '__main__':
     # Сохранение файла
     workbook.save('fields_data.xlsx')
 
-    #end
+def create_and_upload_file(file_name='test.txt', file_content='Hey Dude!'):
+
+    try:
+        drive = GoogleDrive(gauth)
+
+        my_file = drive.CreateFile({'title': f'{file_name}'})
+        my_file.SetContentString(file_content)
+        my_file.Upload()
+
+        return f'File {file_name} was uploaded!Have a good day!'
+    except Exception as _ex:
+        return 'Got some trouble, check your code please!'
+
+def lead_to_googlesheets(*, lead_name, project_name):
+    lead = get_lead(lead_name)
+    fields_data = optimize_lead_to_data(lead)
+
+
+    print(create_and_upload_file(file_name='hello/hello.txt', file_content='Hello Friend'))
+
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Создание элементов первой вкладки
+        self.tab1_label = QLabel('Введи название сделки')
+        self.tab1_input = QLineEdit()
+        self.tab1_button1 = QPushButton('Запустить')
+        self.tab1_button2 = QPushButton('Если не работает')
+
+        # Создание элементов второй вкладки
+        self.tab2_label1 = QLabel('Текстовое поле 1')
+        self.tab2_input1 = QLineEdit()
+        self.tab2_label2 = QLabel('Текстовое поле 2')
+        self.tab2_input2 = QLineEdit()
+        self.tab2_label3 = QLabel('Текстовое поле 3')
+        self.tab2_input3 = QLineEdit()
+        self.tab2_label4 = QLabel('Текстовое поле 4')
+        self.tab2_input4 = QLineEdit()
+        self.tab2_button = QPushButton('Вернуться назад')
+
+        # Создание макетов для элементов первой и второй вкладок
+        tab1_layout = QVBoxLayout()
+        tab1_layout.addWidget(self.tab1_label)
+        tab1_layout.addWidget(self.tab1_input)
+        tab1_layout.addWidget(self.tab1_button1)
+        tab1_layout.addWidget(self.tab1_button2)
+
+        tab2_layout = QVBoxLayout()
+        tab2_layout.addWidget(self.tab2_label1)
+        tab2_layout.addWidget(self.tab2_input1)
+        tab2_layout.addWidget(self.tab2_label2)
+        tab2_layout.addWidget(self.tab2_input2)
+        tab2_layout.addWidget(self.tab2_label3)
+        tab2_layout.addWidget(self.tab2_input3)
+        tab2_layout.addWidget(self.tab2_label4)
+        tab2_layout.addWidget(self.tab2_input4)
+        tab2_layout.addWidget(self.tab2_button)
+
+        # Создание виджета вкладок и добавление в него макетов
+        self.tabs = QTabWidget()
+        self.tabs.addTab(QWidget(), 'Первая вкладка')
+        self.tabs.addTab(QWidget(), 'Вторая вкладка')
+        self.tabs.widget(0).setLayout(tab1_layout)
+        self.tabs.widget(1).setLayout(tab2_layout)
+
+        # Создание макета для главного окна и добавление в него виджета вкладок
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.tabs)
+
+        # Установка главного макета для главного окна
+        self.setLayout(main_layout)
+
+        # Подключение обработчиков событий для кнопок
+        self.tab1_button1.clicked.connect(self.on_tab1_button1_clicked)
+        self.tab1_button2.clicked.connect(self.on_tab1_button2_clicked)
+        self.tab2_button.clicked.connect(self.on_tab2_button_clicked)
+
+    def on_tab1_button1_clicked(self):
+        # Обработчик события для кнопки "Запустить"
+     lead_to_excel(lead_name=self.tab1_input.text(), project_name="1")
+
+    def on_tab1_button2_clicked(self):
+        # Обработчик события для кнопки "Если не работает"
+        self.tabs.setCurrentIndex(1)
+
+
+    def on_tab2_button_clicked(self):
+        # Обработчик события для кнопки "Вернуться назад"
+        self.tabs.setCurrentIndex(0)
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec_())
+
+lead_to_googlesheets(lead_name="ТЕСТ ДЛЯ ДЖУ 2", project_name="None")
+
+
+
